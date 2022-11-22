fa1cce66b340899aee0aac6132a229c90bd63390
vt415769 <mbiyavanga.luvetama@vtal.com>
2022-11-22 09:56:14 -0300

    Security-database

---
+++ security-configure-database/src/main/java/com/securityconfiguredatabase/SecurityConfigureDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.securityconfiguredatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityConfigureDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityConfigureDatabaseApplication.class, args);
+	}
+
+}
---
+++ security-configure-database/src/test/java/com/securityconfiguredatabase/SecurityConfigureDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityconfiguredatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityConfigureDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
